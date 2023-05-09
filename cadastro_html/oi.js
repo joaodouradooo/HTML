ed8c83bc8bd03ea6c999c5c75f69fd6950023dee
João <132951271+joaodouradooo@users.noreply.github.com>
2023-05-08 21:01:20 -0300

    Add files via upload

---
+++ cadastro_html/oi.js
@@ -0,0 +1,29 @@
+function mostrarnome(){
+    var nome=document.getElementById("nome");
+    alert("Bem vindo",nome)}
+    function mudacor(){
+        var div = document.getElementById("container");
+        div.style.backgroundColor = "green";
+    }
+    
+    function mudacor2(){
+        var div2= document.getElementById("nome");
+        div2.style.backgroundColor= "yellow";
+    }
+    
+    function mudacor3(){
+        var div2= document.getElementById("email");
+        div2.style.backgroundColor= "yellow";
+    }
+    
+    function mudacor4(){
+        var div2= document.getElementById("idade");
+        div2.style.backgroundColor= "yellow";
+    }
+    
+    function mudacor5(){
+        var div2= document.getElementById("cidade");
+        div2.style.backgroundColor= "yellow";
+    }
+    
+    
