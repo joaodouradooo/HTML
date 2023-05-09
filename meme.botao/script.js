bee26b3e26da3f19e513565f80d15a20926b40f1
João <132951271+joaodouradooo@users.noreply.github.com>
2023-05-08 21:09:59 -0300

    Add files via upload

---
+++ meme.botao/script.js
@@ -0,0 +1,23 @@
+var song = new Audio()
+function test(value){
+   let musica = value
+switch(musica) {
+    case 1:
+    song.src="tauz.mp3"
+    break;
+
+    case 2:
+    song.src="minato.mp3"
+    break;
+
+    case 3:
+    song.src="borges.mp3"
+    break;
+
+
+
+
+
+}
+song.play()
+}
